32ac9574511b2a98bc728e8c1ac70b1192562687
Brent Dunklau <bdunklau@yahoo.com>
2022-03-03 05:48:20 +0000

    https://headsup-video.atlassian.net/wiki/spaces/HEAD3/pages/423493652/DIY+Video+Chat  DIY video chat

--- public/script.js
+++ public/script.js
@@ -1,56 +1,146 @@
+
+
+/***********************************************************************************************
+See  https://headsup-video.atlassian.net/wiki/spaces/HEAD3/pages/423493652/DIY+Video+Chat
+See  https://headsupvideo.atlassian.net/browse/HEAD3-10
+***********************************************************************************************/
+
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+  secure: true,
+  host: 'testpeer.headsup.video',
+  port: '443'
 })
-const myVideo = document.createElement('video')
+
+const myVideo = createVideoElement()
+
 myVideo.muted = true
+// myVideo.style.width="20px"
+// myVideo.style.height="20px"
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
-}).then(stream => {
-  addVideoStream(myVideo, stream)
+}).then(async(stream) => {
+  await addVideoStream(myVideo, stream, '111111111 async')  
+  
+  console.log('myPeer: ', myPeer)
 
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
+  /***
+  someone is calling ME...
+  How does this get called?
+  myPeer.on('open', id => {}) emits socket:join-room
+  server.js socket:on:join-room listens for "ready" event
+  below: socket.emit("ready")  causes server.js to broadcast : user-connected
+  below: !! in other peers !!  socket.on:user-connected calls connectToNewUser() 
+  connectToNewUser() !! in other peers !! calls  myPeer.call(remoteUserId, ourStream)
+  ***/
+  myPeer.on('call', call => {    
+    const video = createVideoElement()
+    
+    call.answer(stream)   //  ... so I answer
+    logit('call.answer():  ok')
+    
     call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
+      /**
+      YOU ONLY GET THIS WHEN YOU REFRESH FROM PC, NOT FROM IPHONE
+      **/
+      logit('myPeer.on(call, call => {})  STREAM EVENT - THIS IS WHAT WE WANT TO SEE')
+      addVideoStream(video, userVideoStream, '2222222')
     })
+  
+    logit('myPeer.on(call, call => {}) : listening for "close"')
+    call.on('close', () => {
+      logit('myPeer.on(call, call => {}) : remove &lt;video&gt;')
+      video.remove()
+    })
+    
+    logit('myPeer.on(call, call => {}) : call.peer '+call.peer)
+    const remoteUserId = call.peer
+    peers[remoteUserId] = call
   })
 
+  logit('Start listening for user-connected')
   socket.on('user-connected', userId => {
+    logit('user-connected: remote userId = '+userId)
     connectToNewUser(userId, stream)
+    //setTimeout(connectToNewUser,5000,userId,stream)
   })
+  
+  socket.emit("ready")  //  https://stackoverflow.com/questions/66937384/peer-oncalll-is-never-being-called
+  
 })
+// .catch(error => {    
+//   logit(`error: ${error}`)
+// })
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+  logit('user-disconnected: remote userId = '+userId)
+  logit('user-disconnected: peers[userId] = '+peers[userId])
+  if (peers[userId]) peers[userId].close() // WHY UNDEFINED !!!!!!!!!!
 })
 
 myPeer.on('open', id => {
+  logit('myPeer.on("open", id)  id = '+id)
+  //   console.log('myPeer.on("open", id)  id = '+id)
   socket.emit('join-room', ROOM_ID, id)
 })
 
-function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
+// when a remote user connects
+/**
+THIS IS WHAT DOESN'T GET CALLED WHEN IPHONE RELOADS - BUT PC'S OK
+**/
+function connectToNewUser(remoteUserId, ourStream) {
+  const call = myPeer.call(remoteUserId, ourStream)
+  console.log('connectToNewUser: ourStream = ', ourStream)
+  console.log('connectToNewUser: myPeer.call(remoteUserId, ourStream) => call.peer => ', call.peer)
+  logit('connectToNewUser: myPeer.call(remoteUserId, ourStream) where remoteUserId='+remoteUserId)
+  logit('connectToNewUser: myPeer.call(remoteUserId, ourStream) => call.peer => '+ call.peer)
+  
+  const video = createVideoElement()
   call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
+    console.log('connectToNewUser: STREAM NEW REMOTE USER')
+    logit('connectToNewUser: STREAM NEW REMOTE USER')
+    addVideoStream(video, userVideoStream, '3333333')
   })
+  
+  logit('connectToNewUser: listening for "close"')
   call.on('close', () => {
+    logit('connectToNewUser: remove &lt;video&gt;')
     video.remove()
   })
 
-  peers[userId] = call
+  peers[remoteUserId] = call
 }
 
-function addVideoStream(video, stream) {
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
+function createVideoElement() {
+    const myVideo = document.createElement('video')
+    myVideo.setAttribute("controls", "")
+    myVideo.setAttribute("playsinline", "")
+    myVideo.setAttribute("preload", "metadata")
+    return myVideo
+}
+
+function logit(msg) {
+  const logging = document.getElementById('logging')
+  const stuff = document.createElement('div')
+  stuff.innerHTML = msg
+  logging.append(stuff)
+}
+
+async function addVideoStream(video, stream, dbg) {
+    return new Promise((resolve, reject) => {
+        //   console.log('addVideoStream: video: ', video)   // the <video> tag
+        //   console.log('addVideoStream: stream: ', stream) // MediaStream object
+        video.srcObject = stream
+        video.addEventListener('loadedmetadata', () => {
+          logit('addVideoStream  play '+dbg)
+          video.play()
+        })
+        videoGrid.append(video)
+        logit('addVideoStream  resolve() '+dbg)
+        resolve()
+    })
+  
 }
--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const cors = require('cors')
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
+app.use(cors({
+    origin: '*'
+}))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -17,8 +26,14 @@ app.get('/:room', (req, res) => {
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
+    console.log('roomId=', roomId, '  userId=', userId)
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.on('ready',()=>{
+        console.log('join-room: ready: emit user-connected')
+        socket.broadcast.to(roomId).emit('user-connected',userId);
+    })
+    
+    // socket.to(roomId).broadcast.emit('user-connected', userId)
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
@@ -26,4 +41,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+server.listen(9001)
